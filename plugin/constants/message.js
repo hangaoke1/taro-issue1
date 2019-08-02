6c672881bd17ceca01ffe72fcf0d3a7347997092
李健夫 <hzlijianfu@corp.netease.com>
2019-08-02 19:27:31 +0800

    ignore

--- plugin/constants/message.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var namespace = 'MESSAGE';
-
-var PUSH_MESSAGE = exports.PUSH_MESSAGE = namespace + '/PUSH_MESSAGE';
