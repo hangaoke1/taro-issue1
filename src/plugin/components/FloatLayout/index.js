31540fe3c7e0ff06929893fe1c2684f2e23c8b1e
李健夫 <hzlijianfu@corp.netease.com>
2019-08-16 16:56:12 +0800

    增加floatlayout

--- src/plugin/components/FloatLayout/index.js
+++ src/plugin/components/FloatLayout/index.js
@@ -1,5 +1,7 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, ScrollView } from '@tarojs/components'
+import { View, Text, ScrollView } from '@tarojs/components'
+import Iconfont from '../Iconfont';
+import PropTypes from 'prop-types'
 
 import './index.less';
 
@@ -7,14 +9,85 @@ export default class FloatLayout extends Component {
 
     constructor(props) {
         super(props);
+
+        if(props.defaultVisible === undefined){
+            this.state = {
+                visible: false
+            }
+        }else{
+            this.state = {
+                visible: props.defaultVisible
+            }
+        }
     }
 
-    render () {
-        
-        return (
-            <View className="m-FloatLayout">
+    static getDerivedStateFromProps(nextProps, prevState){
+        if(nextProps.defaultVisible === undefined && nextProps.visible != prevState.visible){
+            return {
+                ...prevState,
+                visible: nextProps.visible
+            }
+        }else{
+            return {
+                ...prevState
+            }
+        }
+    }
 
-            </View>
+    onClickMask = () => {
+        const { onClickMask, maskClosable } = this.props;
+
+        if (maskClosable) {
+            this.setState({
+                visible: false
+            })
+        }
+
+        onClickMask && onClickMask();
+    }
+
+    onClose = () => {
+        const { onClose } = this.props;
+
+        this.setState({
+            visible: false
+        })
+
+        onClose && onClose();
+    }
+
+    render() {
+        const { title, contentHeight } = this.props;
+        const { visible } = this.state;
+
+        return (
+            visible ?
+                <View className="m-FloatLayout">
+                    <View className="m-FloatLayout-mask" onClick={this.onClickMask}></View>
+                    <View className="m-FloatLayout-layout">
+                        <View className="layout-header">
+                            {
+                                title ?
+                                    <View className="layout-title">
+                                        <Text>{title}</Text>
+                                        <View className="u-close" onClick={this.onClose}>
+                                            <Iconfont type="icon-close" color="#666" size="36"></Iconfont>
+                                        </View>
+                                    </View> : null
+                            }
+                        </View>
+                        <View className="layout-body">
+                            <ScrollView className="layout-body_content" scrollY={true} style={{
+                                height: contentHeight + 'px'
+                            }}>
+                                <View className="layout-body_content_scroll_body">
+                                    {this.props.children}
+                                </View>
+                            </ScrollView>
+                        </View>
+                    </View>
+                </View>
+                : null
         )
     }
 }
@@ -22,10 +95,18 @@ export default class FloatLayout extends Component {
 
 FloatLayout.defaultProps = {
     title: '',
-    visible: false
+    visible: false,
+    contentHeight: 500,
+    maskClosable: false
 }
+
 
 FloatLayout.propType = {
     title: PropTypes.string,
-    visible: PropTypes.bool
+    visible: PropTypes.bool,
+    contentHeight: PropTypes.number,
+    onClickMask: PropTypes.func,
+    maskClosable: PropTypes.bool,
+    onClose: PropTypes.func,
+    defaultVisible: PropTypes.bool
 }
