3113877025888d4947ad819a5278fd2afcb37e54
hangaoke1 <188869009@qq.com>
2019-09-09 15:52:25 +0800

    perf: 优化气泡节点样式

--- src/plugin/components/Message/im-bot/TplBubbleNodeList/index.js
+++ src/plugin/components/Message/im-bot/TplBubbleNodeList/index.js
@@ -6,30 +6,46 @@ import _get from 'lodash/get';
 import './index.less';
 
 export default function TplBubbleNodeList(props) {
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
   const tpl = _get(props, 'item.content.template', {});
   const nodeLen = _get(tpl, 'list.length', 0);
-  const nodeList = show ? _get(tpl, 'list', []) : _get(tpl, 'list', []).slice(0, 4);
+  const nodeList = show
+    ? _get(tpl, 'list', [])
+    : _get(tpl, 'list', []).slice(0, 4);
 
-  function handleClick () {
-    setShow(true)
+  function handleClick() {
+    setShow(true);
   }
 
-  return <View className="m-bubble-node">
-    <View className="u-label">{_get(tpl, 'label')}</View>
-    <View className="u-title">{_get(tpl, 'title.label')}</View>
-    <View className="u-list">
-      { nodeList.map((node, index) => {
-        return <View className={`u-list-item ${Number(node.p_is_current) === 1 ? 'z-active' : ''}`}>
-          <View className="u-p_title">{node.p_title}</View>
-          <View className="u-p_desc">{node.p_desc}</View>
-          { nodeList.length !== index + 1 ? (<View className="u-line"></View>) : null}
-          <View className="u-dot"></View>
+  return (
+    <View className="m-bubble-node">
+      <View className="u-label">{_get(tpl, 'label')}</View>
+      <View className="u-title">{_get(tpl, 'title.label')}</View>
+      <View className="u-list">
+        {nodeList.map((node, index) => {
+          return (
+            <View
+              className={`u-list-item ${
+                Number(node.p_is_current) === 1 ? 'z-active' : ''
+              }`}
+            >
+              <View className="u-p_title">{node.p_title}</View>
+              <View className="u-p_desc">{node.p_desc}</View>
+              {nodeList.length !== index + 1 ? (
+                <View className="u-line"></View>
+              ) : null}
+              <View className="u-dot"></View>
+            </View>
+          );
+        })}
+      </View>
+      {!show && nodeLen > 4 ? (
+        <View className="u-action" onClick={handleClick}>
+          查看全部
         </View>
-      })}
+      ) : null}
     </View>
-    { !show && nodeLen > 4 ?  <View className="u-action" onClick={handleClick}>查看全部</View> : null}
-  </View>
+  );
 }
 
-TplBubbleNodeList.PropTypes = {}
+TplBubbleNodeList.PropTypes = {};
