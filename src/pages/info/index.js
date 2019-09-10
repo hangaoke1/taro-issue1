618de437680a083914eb1fca6f6231470232c5b6
李健夫 <hzlijianfu@corp.netease.com>
2019-09-10 17:07:31 +0800

    加demo

--- src/pages/info/index.js
+++ src/pages/info/index.js
@@ -8,7 +8,53 @@ const myPluginInterface = Taro.requirePlugin('myPlugin')
 export default class Info extends Component {
 
   config = {
-    navigationBarTitleText: '网易七鱼Demo'
+    navigationBarTitleText: '用户信息'
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [{
+        uid: 'user111111111',
+        name: '用户A',
+        data: [
+          { "key": "real_name", "value": "用户A" },
+          { "key": "mobile_phone", "value": 15669060662 },
+          { "key": "email", "value": "13800000000@163.com" },
+          { "index": 0, "key": "account", "label": "账号", "value": "zhangsan", "href": "http://example.domain/user/zhangsan" },
+          { "index": 1, "key": "sex", "label": "性别", "value": "先生" },
+          { "index": 2, "key": "reg_date", "label": "注册日期", "value": "2015-11-16" },
+          { "index": 3, "key": "last_login", "label": "上次登录时间", "value": "2015-12-22 15:38:54" },
+          { "index": 4, "key": "avatar", "label": "头像", "value": "https://xxxxx.jpg" }
+        ]
+      }, {
+        uid: 'user222222222',
+        name: '用户B',
+        data: [
+          { "key": "real_name", "value": "用户B" },
+          { "key": "mobile_phone", "value": 15669060662 },
+          { "key": "email", "value": "13800000000@163.com" },
+          { "index": 0, "key": "account", "label": "账号", "value": "zhangsan", "href": "http://example.domain/user/zhangsan" },
+          { "index": 1, "key": "sex", "label": "性别", "value": "先生" },
+          { "index": 2, "key": "reg_date", "label": "注册日期", "value": "2015-11-16" },
+          { "index": 3, "key": "last_login", "label": "上次登录时间", "value": "2015-12-22 15:38:54" },
+          { "index": 4, "key": "avatar", "label": "头像", "value": "https://xxxxx.jpg" }
+        ]
+      }, {
+        uid: 'user3333333',
+        name: '用户C',
+        data: [
+          { "key": "real_name", "value": "用户C" },
+          { "key": "mobile_phone", "value": 15669060662 },
+          { "key": "email", "value": "13800000000@163.com" },
+          { "index": 0, "key": "account", "label": "账号", "value": "zhangsan", "href": "http://example.domain/user/zhangsan" },
+          { "index": 1, "key": "sex", "label": "性别", "value": "先生" },
+          { "index": 2, "key": "reg_date", "label": "注册日期", "value": "2015-11-16" },
+          { "index": 3, "key": "last_login", "label": "上次登录时间", "value": "2015-12-22 15:38:54" },
+          { "index": 4, "key": "avatar", "label": "头像", "value": "https://xxxxx.jpg" }
+        ]
+      }]
+    }
   }
 
   componentWillMount() { }
@@ -22,9 +68,34 @@ export default class Info extends Component {
   componentDidHide() { }
 
   render() {
+    const { users } = this.state;
     return (
       <View className='m-Info'>
-        1111
+        <View className="m-Info_item">
+          <View className="m-Info_item_title">
+            <View className="Info_item_title_text">当前用户</View>
+          </View>
+          <View>匿名用户</View>
+        </View>
+        <View className="m-Info_item">
+          <View className="m-Info_item_title">
+            <View className="Info_item_title_text">切换用户</View>
+          </View>
+        </View>
+        <View className="m-Info_btn_con">
+          {
+            users.map(item => {
+              return (
+                <View className="m-Info_btn" key={item.uid}>{item.name}</View>
+              )
+            })
+          }
+        </View>
+        <View className="m-Info_item">
+          <View className="m-Info_item_title">
+            <View className="Info_item_title_text">自定义用户信息</View>
+          </View>
+        </View>
       </View>
     )
   }
