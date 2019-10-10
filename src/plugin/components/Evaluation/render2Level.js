30e4dbd6c167f8209d61147cb1e27e55b7645029
李健夫 <hzlijianfu@corp.netease.com>
2019-10-10 21:28:24 +0800

    YSF-30765  【小程序SDK】（评价）评价按钮：赞、踩、星星---等未选择时icon不对

--- src/plugin/components/Evaluation/render2Level.js
+++ src/plugin/components/Evaluation/render2Level.js
@@ -1,5 +1,5 @@
 import Taro from '@tarojs/taro';
-import { View, Text } from '@tarojs/components';
+import { View, Text,Image } from '@tarojs/components';
 import Iconfont from '../Iconfont';
 
 import './index.less';
@@ -22,13 +22,23 @@ export default function Render2Level(props) {
                 item.value == 100 ?
                   <View className="u-evaluation-icon"
                     onClick={handleClick.bind(undefined, item.tagList, item.value, item.name)}>
-                    <Iconfont type="icon-zan-offx" color={selectValue == 100 ? "#FFAF0F" : "#666"} size='26' />
+                      {
+                        selectValue == 100 ?
+                        <Iconfont type="icon-zan-offx" color={selectValue == 100 ? "#FFAF0F" : "#666"} size='26' />
+                        :
+                        <Image className="image" src='http://nos.netease.com/ysf/24ec7f276b8bb1d96c423aab742ccbe4'></Image>
+                      }
                   </View> : null
               }
               {
                 item.value == 1 ?
                   <View className="u-evaluation-icon" onClick={handleClick.bind(undefined, item.tagList, item.value, item.name)}>
-                    <Iconfont type="icon-unzan-offx" color={selectValue == 1 ? "#FFAF0F" : "#666"} size='26' />
+                    {
+                      selectValue == 1 ?
+                      <Iconfont type="icon-unzan-offx" color={selectValue == 1 ? "#FFAF0F" : "#666"} size='26' />
+                      :
+                      <Image className="image" src='http://nos.netease.com/ysf/124e96661fe2a17ad7998fb700dc9aa0'></Image>
+                    }
                   </View> : null
               }
             </View>
--- src/plugin/components/Evaluation/render345Level.js
+++ src/plugin/components/Evaluation/render345Level.js
@@ -1,5 +1,5 @@
 import Taro from '@tarojs/taro';
-import { View } from '@tarojs/components';
+import { View,Image } from '@tarojs/components';
 import Iconfont from '../Iconfont';
 
 import './index.less';
@@ -18,8 +18,13 @@ export default function Render345Level(props) {
           return (
             <View className="m-evaluation_icon_content" key={item.value}>
               <View className="u-evaluation-icon" onClick={handleClick.bind(undefined, item.tagList, item.value, item.name)}>
-                <Iconfont type="icon-star-offx"
+                {
+                  item.value <= selectValue ?
+                  <Iconfont type="icon-star-offx"
                   color={item.value <= selectValue ? "#FFAF0F" : "#666"} size='26' />
+                  :
+                  <Image className="image" src='http://nos.netease.com/ysf/d5535ee55b17e1e70c70f2881f85b087'></Image>
+                }
               </View>
             </View>
           )
