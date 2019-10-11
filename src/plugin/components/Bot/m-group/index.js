d55adb4ea4dd1e60a5eb262bd39fad8abe7343f3
hangaoke1 <188869009@qq.com>
2019-10-11 17:18:14 +0800

    YSF-30824  【小程序sdk】抽屉列表和气泡列表中item列表type0的数据，无图片的时候，内容没有左侧顶格显示

--- src/plugin/components/Bot/m-group/index.js
+++ src/plugin/components/Bot/m-group/index.js
@@ -4,15 +4,11 @@ import _get from 'lodash/get';
 import './index.less';
 
 export default function MGroup(props) {
-
   const item = _get(props, 'item', {});
 
   return (
     <View className="m-group">
-      <Image
-        className="u-img"
-        src={item.p_img}
-      ></Image>
+      {item.p_img ? <Image className="u-img" src={item.p_img}></Image> : null}
       <View className="u-title">{item.p_title}</View>
     </View>
   );
