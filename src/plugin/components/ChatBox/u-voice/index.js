068f52b42f0825f28a0f97f8adac601a55f0d067
hangaoke1 <188869009@qq.com>
2019-11-08 17:15:04 +0800

    feat: 增加录音倒计时逻辑

--- src/plugin/components/ChatBox/u-voice/index.js
+++ src/plugin/components/ChatBox/u-voice/index.js
@@ -2,9 +2,7 @@ import Taro, { Component } from '@tarojs/taro';
 import { connect } from '@tarojs/redux';
 import { View, Text } from '@tarojs/components';
 import Iconfont from '@/components/Iconfont';
-import {
-  sendVoice
-} from '../../../actions/chat';
+import { sendVoice } from '../../../actions/chat';
 import './index.less';
 
 @connect(
@@ -18,10 +16,12 @@ import './index.less';
 class UVoice extends Component {
   state = {
     showToast: false,
-    showCancel: false
+    showCancel: false,
+    duration: 60
   };
 
   constructor() {
+    this.durationTimer = null;
     this.startTimer = null;
     this.startPos = 0;
     this.endPos = 0;
@@ -33,81 +33,111 @@ class UVoice extends Component {
 
   componentDidMount() {}
 
-  handleTouchStart = (event) => {
+  startTimeDown = () => {
+    this.durationTimer = setInterval(() => {
+      if (this.state.duration > 1) {
+        this.setState(state => ({
+          duration: state.duration - 1
+        }));
+      } else {
+        this.handleTouchEnd();
+        this.stopTimeDown();
+      }
+    }, 1000);
+  };
+
+  stopTimeDown = () => {
+    clearInterval(this.durationTimer);
+    this.setState({
+      duration: 60
+    });
+  };
+
+  handleTouchStart = event => {
     this.startTimer = setTimeout(() => {
-      this.startPos = event.touches[0].clientY
-      this.endPos = event.touches[0].clientY
-      this.cancel = false
+      this.startTimeDown();
+      this.startPos = event.touches[0].clientY;
+      this.endPos = event.touches[0].clientY;
+      this.cancel = false;
       this.setState({
         showToast: true
       });
-      this.RecorderManager = Taro.getRecorderManager()
-      this.RecorderManager.onStop((res) => {
+      this.RecorderManager = Taro.getRecorderManager();
+      this.RecorderManager.onStop(res => {
         if (this.cancel) {
-          console.log('取消发送')
+          console.log('取消发送');
         } else {
-          console.log('录音完成', res)
-          this.props.sendVoice(res)
+          console.log('录音完成', res);
+          this.props.sendVoice(res);
         }
-      })
+      });
       this.RecorderManager.start({
         format: 'mp3',
-        duration: 600000 // 10分钟
-      })
-      
+        duration: 60000 // 1分钟
+      });
+
       console.log('开始录入');
     }, 300);
   };
 
-  handleTouchMove = (event) => {
-    this.endPos = event.touches[0].clientY
-    const diff = this.startPos - this.endPos
+  handleTouchMove = event => {
+    this.endPos = event.touches[0].clientY;
+    const diff = this.startPos - this.endPos;
     this.setState({
       showCancel: diff > 100 ? true : false
-    })
-    event.stopPropagation()
-  }
+    });
+    event.stopPropagation();
+  };
 
-  handleTouchEnd = (event) => {
+  handleTouchEnd = event => {
     clearTimeout(this.startTimer);
+    this.stopTimeDown();
     this.startTimer = null;
     this.setState({
       showToast: false,
       showCancel: false
     });
-    const diff = this.startPos - this.endPos
+    const diff = this.startPos - this.endPos;
     if (diff > 100) {
-      this.cancel = true
-      this.RecorderManager.stop()
+      this.cancel = true;
+      this.RecorderManager.stop();
     } else {
-      this.RecorderManager.stop()
+      this.RecorderManager.stop();
     }
   };
 
-  handleTouchcancel = (event) => {
+  handleTouchcancel = event => {
     clearTimeout(this.startTimer);
     this.startTimer = null;
     this.setState({
       showToast: false,
       showCancel: false
     });
-    this.cancel = true
-    this.RecorderManager.stop()
-  }
+    this.cancel = true;
+    this.RecorderManager.stop();
+  };
 
-  preventTouchMove = (e) => {
-    e.stopPropagation()
-    return
-  }
+  preventTouchMove = e => {
+    e.stopPropagation();
+    return;
+  };
 
   render() {
-    const { showToast, showCancel } = this.state;
+    const { showToast, showCancel, duration } = this.state;
     let toast = null;
     if (showToast && !showCancel) {
       toast = (
         <View className="u-voice-wrap" onTouchMove={this.preventTouchMove}>
           <View className="u-voice">
-            <Iconfont type="icon-chat-voice" color="#fff" size="90"></Iconfont>
+            {duration > 10 ? (
+              <Iconfont
+                type="icon-chat-voice"
+                color="#fff"
+                size="90"
+              ></Iconfont>
+            ) : (
+              <View style="font-size: 90px;color:#fff;">{duration}</View>
+            )}
             <Text className="u-tip">手指上滑，取消发送</Text>
           </View>
         </View>
@@ -117,7 +147,15 @@ class UVoice extends Component {
       toast = (
         <View className="u-voice-wrap" onTouchMove={this.preventTouchMove}>
           <View className="u-voice">
-            <Iconfont type="icon-chat-cancel" color="#fff" size="90"></Iconfont>
+            {duration > 10 ? (
+              <Iconfont
+                type="icon-chat-cancel"
+                color="#fff"
+                size="90"
+              ></Iconfont>
+            ) : (
+              <View style="font-size: 90px;color:#fff;">{duration}</View>
+            )}
             <Text className="u-tip">松开手指，取消发送</Text>
           </View>
         </View>
@@ -125,14 +163,18 @@ class UVoice extends Component {
     }
     return (
       <Button
-        className={`u-voice-input ${showToast ? 'z-active': ''}`}
+        className={`u-voice-input ${showToast ? 'z-active' : ''}`}
         onTouchStart={this.handleTouchStart}
         onTouchEnd={this.handleTouchEnd}
         onTouchMove={this.handleTouchMove}
         onTouchcancel={this.handleTouchcancel}
       >
-        { showToast && !showCancel ? <Text>松开 结束</Text> : <Text>按住 说话</Text>}
-        { toast }
+        {showToast && !showCancel ? (
+          <Text>松开 结束</Text>
+        ) : (
+          <Text>按住 说话</Text>
+        )}
+        {toast}
       </Button>
     );
   }
