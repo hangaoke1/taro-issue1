e6e26c78b10fdb9e460819629d29c391ad7e22b4
hangaoke1 <188869009@qq.com>
2020-02-03 19:25:33 +0800

    feat: update

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,8 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 
+const myPluginInterface = Taro.requirePlugin('myPlugin')
+
 class index extends Component {
   config = {
     navigationBarTitleText: '首页'
--- src/plugin/global_config.js
+++ src/plugin/global_config.js
@@ -1,63 +1,9 @@
 import Taro from '@tarojs/taro';
-import NIM from './vendors/nim/NIM_Web_NIM_weixin_v6.8.0'
-import NIM_TEST from './vendors/nim/NIM_Web_NIM_weixin.test.min';
 
-
-export const initDeviceid = (isReset) => {
-  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'; // 随机数种子
-  const deviceid = [];
-  let ret = null;
-
-  ret = Taro.getStorageSync('YSF-DEVICEID');
-
-  if (isReset || !ret) {
-    for (let i = 0, n; i < 20; ++i) {
-      n = Math.floor(Math.random() * chars.length);
-      deviceid.push(chars.charAt(n));
-    }
-    ret = deviceid.join('').toLowerCase();
-    Taro.setStorageSync(
-      'YSF-DEVICEID', ret
-    )
-  }
-  return ret;
-}
-
-const globalConfig = {
-  appKey: '',
-  domain: 'https://qiyukf.com',
-  account: '',
-  bid: '-1',
-  token: '',
-  deviceid: initDeviceid(),
-  foreignid: '',
-  userInfo: null,
-  product: null,
-  staffid: '',
-  groupid: '',
-  level: 0,
-  title: '在线客服',
-  bundleid: Taro.getAccountInfoSync().miniProgram.appId,
-  heartbeatCycle: 8000,
-  store: null,
-  NIM: NIM,
-  autoCopy: true, // 处理链接点击时，是否进行复制
-  fullScreen: false // 是否为全面屏模式
-};
+const globalConfig = {};
 
 export const get = (key) => globalConfig[key];
 
 export const set = (key, value) => {
-  if (process.env.NODE_ENV === 'development') {
-    if (key == 'domain') {
-      if (value == 'https://qytest.netease.com') {
-        globalConfig['NIM'] = NIM_TEST;
-        console.log(NIM_TEST);
-      } else {
-        globalConfig['NIM'] = NIM;
-        console.log(NIM);
-      }
-    }
-  }
   globalConfig[key] = value;
 }
